7d327e7b6127b9b7fa2f98988b9da476b29972f5
anton-sales <anton.arus@gmail.com>
2018-09-05 12:35:22 +1200

    refactored code

--- index.js
+++ index.js
@@ -1,46 +1,30 @@
-function decryptMe(et) {
-    let result = []
-    et = et.split('')
-    let map = {
-        a: 'k',
-        b: 'n',
-        c: 'y',
-        d: 'j',
-        e: 'b',
-        f: 's',
-        g: 't',
-        h: 'd',
-        i: 'f',
-        j: 'e',
-        k: 'h',
-        l: 'u',
-        m: 'z',
-        n: 'o',
-        o: 'a',
-        p: 'c',
-        q: 'v',
-        r: 'x',
-        s: 'w',
-        t: 'q',
-        u: 'm',
-        v: 'r',
-        w: 'l',
-        x: 'i',
-        y: 'p',
-        z: 'g',
-        ',': ',',
-        ' ': ' '
-    }
-    for (let i = 0; i < et.length; i++) {
-        let position = et[i]
-        result.push(map[position])
-    }
-    return result.join('')
+const alph = 'abcdefghijklmnopqrstuvwxyz'.split('')
+
+const decrypt = (encrypt, cipher) => //function start
+    encrypt.split('').map(char => //map the characters in the encrypted string
+        cipher.split('').indexOf(char) != -1 //does the character have an encryption index?
+            ? alph[cipher.split('').indexOf(char)] //if so, substitute the char with og aplhabet
+            : char //if not found, don't substitue the character (commas, spaces, etc)
+    ).join('') //join the char array into a single string
+
+
+const encrypt = (string, cipher) =>
+    string.split('').map(char => 
+        alph.indexOf(char) != -1 //does the character have an encryption index?
+            ? cipher.split('')[alph.indexOf(char)] //if so, substitute the char with og aplhabet
+            : char
+    ).join('')
+
+// cipher = 'oephjizkxdawubnytvfglqsrcm'
+// encrypt = 'knlfgnb, sj koqj o yvnewju'
+
+const encrypt1 = 'knlfgnb, sj koqj o yvnewju'
+const cipher1 = 'oephjizkxdawubnytvfglqsrcm'
+
+
+console.log(decrypt(encrypt1, cipher1))
+
+module.exports = {
+    decrypt,
+    encrypt
 }
-
-let encryptedText = 'knlfgnb, sj koqj o yvnewju'
-
-decryptMe(encryptedText)
-
-module.exports = decryptMe
-
--- index.test.js
+++ index.test.js
@@ -1,10 +1,62 @@
-const decryptMe = require('./index')
+const {decrypt, encrypt} = require('./index')
 
-test('the code is houston we have a problem', () => {
-    let encryptedText = 'knlfgnb, sj koqj o yvnewju'
-    let expected = 'houston, we have a problem'
+test('example 1', () => {
+    const cipher = 'zodvqukgwefbyitmrsplhacxnj'
+    const encrypt = 'dzs'
+    const expected = 'car'
 
-    let actual = decryptMe(encryptedText)
+    const actual = decrypt(encrypt, cipher)
 
     expect(actual).toBe(expected)
 })
+
+test('example 1 encrypt', () => {
+    const cipher = 'zodvqukgwefbyitmrsplhacxnj'
+    const string = 'car'
+    const expected = 'dzs'
+
+    const actual = encrypt(string, cipher)
+
+    expect(actual).toBe(expected)
+})
+
+test('example 2', () => {
+    const cipher = 'xipmuzfkbrlwotjancqgveshdy'
+    const encrypt = 'skd qj qucbjvq?'
+    const expected = 'why so serious?'
+
+    const actual = decrypt(encrypt, cipher)
+
+    expect(actual).toBe(expected)
+})
+
+test('example 2 encrypt', () => {
+    const cipher = 'xipmuzfkbrlwotjancqgveshdy'
+    const string = 'why so serious?'
+    const expected = 'skd qj qucbjvq?'
+
+    const actual = encrypt(string, cipher)
+
+    expect(actual).toBe(expected)
+})
+
+test('Challenge', () => {
+    const cipher = 'oephjizkxdawubnytvfglqsrcm'
+    const encrypt = 'knlfgnb, sj koqj o yvnewju'
+    const expected = 'houston, we have a problem'
+
+    const actual = decrypt(encrypt, cipher)
+
+    expect(actual).toBe(expected)
+})
+
+
+test('Challenge encrypt', () => {
+    const cipher = 'oephjizkxdawubnytvfglqsrcm'
+    const string = 'houston, we have a problem'
+    const expected = 'knlfgnb, sj koqj o yvnewju'
+
+    const actual = encrypt(string, cipher)
+
+    expect(actual).toBe(expected)
+})  
